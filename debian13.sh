12c8f5c12603b699e8fc156a05493617c4450632
franckfpro <franckfpro@localhost.lan>
2026-06-22 14:16:48 +0200

    more flatpak

--- debian13.sh
+++ debian13.sh
@@ -9,18 +9,21 @@ apt update
 apt dist-upgrade
 
 apt install --assume-yes flatpak
+flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --assumeyes fedora com.github.tchx84.Flatseal
+flatpak install --assumeyes fedora org.chromium.Chromium
+flatpak install --assumeyes fedora org.gabmus.gfeeds
+flatpak install --assumeyes fedora org.keepassxc.KeePassXC
+flatpak install --assumeyes fedora org.vim.Vim
 flatpak install --assumeyes flathub com.github.unrud.VideoDownloader
+flatpak install --assumeyes flathub com.vscodium.codium
+flatpak install --assumeyes flathub io.gitlab.news_flash.NewsFlash
+flatpak install --assumeyes flathub io.mpv.Mpv
+flatpak install --assumeyes flathub io.neovim.nvim
 
-apt install --assume-yes neovim
 apt install --assume-yes newsboat
 apt install --assume-yes ranger
-apt install --assume-yes mpv
-apt install --assume-yes ffmpeg
-apt install --assume-yes chromium
-apt install --assume-yes keepassxc
-
-ls .local/share/applications/chromium-browser.desktop
 
 select-editor
 
