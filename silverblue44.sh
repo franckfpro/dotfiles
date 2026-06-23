378ab7ec8a7b3dca24821c46a7ca34293b652811
franckfpro <franckfpro@localhost.lan>
2026-06-23 18:28:27 +0200

    brave time

--- silverblue44.sh
+++ silverblue44.sh
@@ -3,14 +3,13 @@
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --assumeyes fedora com.github.tchx84.Flatseal
 flatpak install --assumeyes fedora org.keepassxc.KeePassXC
-flatpak install --assumeyes fedora org.chromium.Chromium
 flatpak install --assumeyes flathub com.github.unrud.VideoDownloader
 flatpak install --assumeyes flathub com.google.Chrome
 flatpak install --assumeyes flathub com.vscodium.codium
 flatpak install --assumeyes flathub io.gitlab.news_flash.NewsFlash
 flatpak install --assumeyes flathub io.mpv.Mpv
 flatpak install --assumeyes flathub io.neovim.nvim
-flatpak install --assumeyes flathub io.github.ungoogled_software.ungoogled_chromium
+flatpak install --assumeyes flathub com.brave.Browser
 
 flatpak uninstall --assumeyes org.fedoraproject.MediaWriter
 flatpak uninstall --assumeyes org.gnome.Calculator
@@ -46,8 +45,7 @@ tar xvf ranger-stable.tar.gz
 
 mkdir ~/.config/autostart
 cp /var/lib/flatpak/app/io.gitlab.news_flash.NewsFlash/current/active/export/share/applications/io.gitlab.news_flash.NewsFlash.desktop ~/.config/autostart/
-cp /var/lib/flatpak/app/io.github.ungoogled_software.ungoogled_chromium/current/active/export/share/applications/io.github.ungoogled_software.ungoogled_chromium.desktop ~/.config/autostart/
-cp /var/lib/flatpak/app/org.chromium.Chromium/current/active/export/share/applications/org.chromium.Chromium.desktop ~/.config/autostart/
+cp /var/lib/flatpak/app/com.brave.Browser/current/active/export/share/applications/com.brave.Browser.desktop ~/.config/autostart/
 
 echo "
 [Desktop Entry]
